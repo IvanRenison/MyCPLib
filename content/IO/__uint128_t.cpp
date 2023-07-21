9a5bc886ae049c46d127cce784fc15cf62d8de09
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-07-21 15:55:08 -0300

    Add >> and << for __uint128_t

---
+++ content/IO/__uint128_t.cpp
@@ -0,0 +1,16 @@
+#include <IncludeTemplate.hpp>
+
+istream& operator>>(istream& is, __uint128_t& value) {
+  unsigned long long x;
+  is >> x;
+  value = x;
+  return is;
+}
+
+ostream& operator<<(ostream& os, __uint128_t value) {
+  if (value >= 10) {
+    os << (value / 10);
+  }
+  os << (char)(value % 10 + '0');
+  return os;
+}
