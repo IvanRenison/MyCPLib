64d991c8e9aeb061979a8cf776561b6cee41fb7c
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-07-23 09:40:51 -0300

    Fix primes sieve

--- content/math/Primes.cpp
+++ content/math/Primes.cpp
@@ -6,8 +6,8 @@
 
 vector<bool> prime(MAXN, true);
 void sieve(void) {
-  prime[1] = false;
-  for (ull i = 0; i < MAXN; i += 2) {
+  prime[0] = false, prime[1] = false;
+  for (ull i = 4; i < MAXN; i += 2) {
     prime[i] = false;
   }
 
