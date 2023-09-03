926cf7ea8f7fe03422cfa95987346ecd240d74ca
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-09-03 12:06:57 -0300

    Add functions to Ratio

--- content/math/Rational.cpp
+++ content/math/Rational.cpp
@@ -24,6 +24,10 @@ struct Ratio {
   Ratio operator*(Ratio o) const {
     return Ratio(num * o.num, den * o.den);
   }
+  Ratio operator/(Ratio o) const {
+    assert(o.num != 0);
+    return Ratio(num * (ll)o.den * signum(o), den * abs(o.num));
+  }
 
   void operator+=(Ratio o) {
     *this = *this + o;
@@ -34,12 +38,27 @@ struct Ratio {
   void operator*=(Ratio o) {
     *this = *this * o;
   }
+  void operator/=(Ratio o) {
+    *this = *this / o;
+  }
 
   bool operator==(Ratio o) const {
     return num == o.num && den == o.den;
   }
   bool operator!=(Ratio o) const {
     return !(*this == o);
+  }
+  bool operator<(Ratio o) const {
+    return num * (ll)o.den < o.num * (ll)den;
+  }
+  bool operator<=(Ratio o) const {
+    return *this < o || *this == o;
+  }
+  bool operator>(Ratio o) const {
+    return o < *this;
+  }
+  bool operator>=(Ratio o) const {
+    return o <= *this;
   }
 
   friend ostream& operator<<(ostream& os, Ratio r) {
@@ -50,3 +69,7 @@ struct Ratio {
     return os;
   }
 };
+
+ll signum(Ratio n) {
+  return signum(n.num);
+}
