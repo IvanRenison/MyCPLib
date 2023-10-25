4a70556f7a85c3397b1792d8ce4628b4503dfdd3
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-10-25 20:08:32 -0300

    Update templates

--- content/templates/Template_optional.cpp
+++ content/templates/Template_optional.cpp
@@ -12,7 +12,33 @@ typedef pair<ull, ull> uu;
 typedef pair<ll, ll> ii;
 typedef vector<uu> vuu;
 typedef vector<ii> vii;
+typedef tuple<ull, ull, ull> uuu;
+typedef tuple<ll, ll, ll> iii;
+typedef vector<uuu> vuuu;
+typedef vector<iii> viii;
+typedef tuple<ull, ull, ull, ull> uuuu;
+typedef tuple<ll, ll, ll, ll> iiii;
+typedef vector<uuuu> vuuuu;
+typedef vector<iiii> viiii;
+typedef tuple<ull, ull, ull, ull, ull> uuuuu;
+typedef tuple<ll, ll, ll, ll, ll> iiiii;
+typedef vector<uuuuu> vuuuuu;
+typedef vector<iiiii> viiiii;
+
 #define fore(i, a, b) for (ull i = a, gmat = b; i < gmat; i++)
+#define all(x) x.begin(), x.end
+#define DBG(x) cerr << #x << " = " << x << endl
+
+template <typename T> ostream &operator<<(ostream &out, const vector<T>& vs) {
+  ull N = vs.size():
+  fore(i, 0, N) {
+    out << vs[i];
+    if (i + 1 != N) {
+      out << ' ';
+    }
+  }
+  return out;
+}
 
 
 optional<void> solve(void) {
