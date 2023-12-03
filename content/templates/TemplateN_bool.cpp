86a2a7cbd93f3ec3f9d103dff37ebeb5fa52aadb
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-12-03 14:08:38 -0300

    Reorganice and add new `typedef`s in templates

--- content/templates/TemplateN_bool.cpp
+++ content/templates/TemplateN_bool.cpp
@@ -5,26 +5,34 @@
 using namespace std;
 
 typedef unsigned long long ull;
-typedef long long ll;
 typedef vector<ull> vu;
-typedef vector<ll> vi;
 typedef pair<ull, ull> uu;
-typedef pair<ll, ll> ii;
-typedef vector<uu> vuu;
-typedef vector<ii> vii;
 typedef tuple<ull, ull, ull> uuu;
-typedef tuple<ll, ll, ll> iii;
+typedef vector<uu> vuu;
 typedef vector<uuu> vuuu;
-typedef vector<iii> viii;
 typedef tuple<ull, ull, ull, ull> uuuu;
-typedef tuple<ll, ll, ll, ll> iiii;
 typedef vector<uuuu> vuuuu;
-typedef vector<iiii> viiii;
 typedef tuple<ull, ull, ull, ull, ull> uuuuu;
-typedef tuple<ll, ll, ll, ll, ll> iiiii;
 typedef vector<uuuuu> vuuuuu;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<ll, ll> ii;
+typedef vector<ii> vii;
+typedef tuple<ll, ll, ll> iii;
+typedef vector<iii> viii;
+typedef tuple<ll, ll, ll, ll> iiii;
+typedef vector<iiii> viiii;
+typedef tuple<ll, ll, ll, ll, ll> iiiii;
 typedef vector<iiiii> viiiii;
 typedef long double ld;
+typedef vector<ld> vd;
+typedef pair<ld, ld> dd;
+typedef vector<dd> vdd;
+typedef tuple<ld, ld, ld> ddd;
+typedef vector<ddd> vddd;
+typedef tuple<ld, ld, ld, ld> dddd;
+typedef vector<dddd> vdddd;
+typedef vector<bool> vb;
 
 #define fore(i, a, b) for (ull i = a, gmat = b; i < gmat; i++)
 #define all(x) x.begin(), x.end()
