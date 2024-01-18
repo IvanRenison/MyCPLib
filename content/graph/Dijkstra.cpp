66e3ae5502e04ba1bf952b6db058d944bc10a8b6
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-01-18 18:45:09 -0300

    Remove `struct Graph` and related

--- content/graph/Dijkstra.cpp
+++ content/graph/Dijkstra.cpp
@@ -2,34 +2,34 @@
 
 #include <IncludeTemplate.hpp>
 
-#include <Graph.cpp>
 #include <Reverse_priority_queue.cpp>
 
 /* The return value is a vector of size n, where the i-th element is the
  * vertex that precedes the i-th vertex in the shortest path from s to i.
- * If there is no such path, the i-th element is nullopt.
+ * If there is no such path, the i-th element is inf.
+ * Also the s-th element is inf.
  */
-typedef ull Distance;
-vector<optional<Vertex>> Dijkstra(WeightedGraph g, Vertex s) {
-  vector<optional<Vertex>> prev(g.n);
-  vector<Distance> dist(g.n, numeric_limits<Distance>::max());
-  dist[s] = 0;
+typedef ull Dis;
+vu Dijkstra(vector<vuu> adj, ull s) {
+  ull n = adj.size();
+  vu prev(n, inf);
+  vector<Dis> dist(n, numeric_limits<Dis>::max());
 
-  rpriority_queue<tuple<Distance, Vertex, Vertex>> q;
+  rpriority_queue<tuple<Dis, ull, ull>> q;
 
-  q.push({0, s, s});
+  q.push({0, s, inf});
 
   while (!q.empty()) {
     auto [d, v, by] = q.top();
     q.pop();
-    if (prev[v].has_value()) {
+    if (prev[v] != inf) {
       continue;
     }
 
     prev[v] = by, dist[v] = d;
 
-    for (auto [u, w] : g.adj[v]) {
-      if (!prev[u].has_value()) {
+    for (auto [u, w] : adj[v]) {
+      if (prev[u] == inf) {
         q.push({d + w, u, v});
       }
     }
